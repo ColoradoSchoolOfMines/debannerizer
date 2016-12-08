6f89d27436e9b24c78db9923231c61bee4581329
Jack Rosenthal <jack@rosenth.al>
2016-12-07 18:29:46 -0700

    database bindings and config

---
+++ debannerizer/db.py
@@ -0,0 +1,14 @@
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+__all__ = ['engine', 'Session', 'Base', 'session']
+
+with open('config.yaml') as f:
+    config_yaml = yaml.load(f)
+
+engine = create_engine(config_yaml['db'], echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
